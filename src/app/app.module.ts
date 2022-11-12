bee83b58627ad06170f603a09a6bf78626b6037e
Paula Fahmy <paula@nacitadrive.com>
2022-11-12 19:17:44 +0200

    Added routing module to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app.routing-module";
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
     // New components generated from CLI automatically added here
     declarations: [AppComponent, CartComponent],
-    imports: [BrowserModule, FormsModule, BooksModule],
+    imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
     bootstrap: [AppComponent],
 })
 export class AppModule{}
